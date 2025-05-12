0ed6636b2ddbaf86e00ab8c174c8361fbec6e72c
Simon <34662922+MadPanCake@users.noreply.github.com>
2025-05-12 16:19:39 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/category/unlimited-adventures">
+            to="/installation">
             Get Started!
           </Link>
         </div>
